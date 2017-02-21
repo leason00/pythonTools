508e974798deaa83740d7f150c4ba02c60f25f42
leason00 <22647085@qq.com>
2017-02-21 21:55:08 +0800

    bat

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import subprocess
+import commands
+import os
+
+# aaa = commands.getoutput("java")
+# # bbb = commands.getstatus("echo 'lll'")
+# print aaa
+# # print bbb
+aaa = 'nihao'
+# bbb = os.system('leason.bat '+aaa)
+bbb = commands.getoutput('leason.bat '+aaa)
+print bbb
